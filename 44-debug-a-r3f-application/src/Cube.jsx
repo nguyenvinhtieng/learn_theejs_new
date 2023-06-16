81eaa632b9101e9de49b6b516bfd71d61d1128f1
VinhTieng <vinhtieng123@gmail.com>
2023-06-16 23:18:45 +0700

    add lesson

---
+++ 44-debug-a-r3f-application/src/Cube.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Cube({scale = 1}) {
+  return (
+    <mesh position-x={ 2 } scale={ scale }>
+    <boxGeometry />
+    <meshStandardMaterial color="mediumpurple" />
+</mesh>
+  )
+}
